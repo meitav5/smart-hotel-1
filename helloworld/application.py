451551213752242db0fc8f3b90a365ef60606186
EC2 Default User <ec2-user@ip-172-31-38-150.ec2.internal>
2023-09-02 21:39:39 +0000

    redirect url changed.

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,13 +1,15 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+from flask import Flask, render_template, request, redirect, Response
 from helloworld.flaskrun import flaskrun
+import os
 
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    url = os.environ.get('AWS_STATIC_WEBSITE_S3_URL')
+    return redirect(url)
 
 @application.route('/', methods=['POST'])
 def post():
