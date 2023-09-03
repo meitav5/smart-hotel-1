3352062329ea176bd0fe2531c0a81f8b7a1fcd2d
meitav5 <meitav102@gmail.com>
2023-09-03 21:34:19 +0000

    login fixed.

--- helloworld/aws.py
+++ helloworld/aws.py
@@ -7,8 +7,8 @@ import json
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-AWS_SECRET_ACCESS_KEY = "AKIA2HA3C4EPQI2TDR5B"
-AWS_ACCESS_KEY_ID = "525cvr4FPKr/3l7jYxmRAcM0XFP56EeKczChE33D"
+AWS_ACCESS_KEY_ID = "AKIA2HA3C4EPQI2TDR5B"
+AWS_SECRET_ACCESS_KEY = "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"
 AWS_REGION_NAME = "us-east-1"
 
 dynamodb = boto3.client(
